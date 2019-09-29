88ccec097c2f72cc2eed0117e39316ac079eb678
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2019-09-29 11:55:11 +0200

    Added automatic downloading with GDrive CLI

--- GetFreeCourses-Automatic-Decoder.ps1
+++ GetFreeCourses-Automatic-Decoder.ps1
@@ -2,53 +2,69 @@
 # Find 7-Zip executable
 # -.-.-.-.-.-.-.-.-.-.-.-.-
 
-$7Zip = gci c:\Program* -include '7z.exe' -recurse -ea 4|select -first 1 -expand fullname
+$7Zip = Get-ChildItem c:\Program* -include '7z.exe' -recurse -ea 4 | Select-Object -first 1 -expand fullname
 
 # -.-.-.-.-.-.-.-.-.-.-.-.-
 # Loop through all containers
 # -.-.-.-.-.-.-.-.-.-.-.-.-
 
 $files = Get-ChildItem -Filter *.zip
-foreach ($file in $files){
+foreach ($file in $files) {
 
-$fileBaseName = $file.BaseName;
-# Write-Host "Files = $fileBaseName";
+    $fileBaseName = $file.BaseName;
+    # Write-Host "Files = $fileBaseName";
 
 
-# -.-.-.-.-.-.-.-.-.-.-.-.-
-# Extract desired txt file
-# -.-.-.-.-.-.-.-.-.-.-.-.-
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
+    # Extract desired txt file
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
 
-& $7Zip e $file "$fileBaseName\$fileBaseName.txt" -r -y
-# Write-Host "$fileBaseName.txt";
+    & $7Zip e $file "$fileBaseName\$fileBaseName.txt" -r -y
+    # Write-Host "$fileBaseName.txt";
 
-clear
+    Clear-Host
 
-# -.-.-.-.-.-.-.-.-.-.-.-.-
-# Decode Base64
-# -.-.-.-.-.-.-.-.-.-.-.-.-
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
+    # Decode Base64
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
 
-$base64code = (Get-Content "$fileBaseName.txt")[2]
-# Write-Host "Base64 is = $base64code";
+    $base64code = (Get-Content "$fileBaseName.txt")[2]
+    # Write-Host "Base64 is = $base64code";
 
-$browserURL = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("$base64code"))
-Write-Host "Browser url is = $browserURL";
+    $browserURL = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("$base64code"))
+    Write-Host "Browser url is = $browserURL";
 
-# -.-.-.-.-.-.-.-.-.-.-.-.-
-# Remove useless files
-# -.-.-.-.-.-.-.-.-.-.-.-.-
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
+    # Remove useless files
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
 
-Remove-Item "$fileBaseName.txt"
-# Remove-Item "$file"
+    Remove-Item "$fileBaseName.txt"
+    # Remove-Item "$file"
 
-# -.-.-.-.-.-.-.-.-.-.-.-.-
-# Open browser with the link
-# -.-.-.-.-.-.-.-.-.-.-.-.-
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
+    # Get the File ID from the google Drive url
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
 
-Start-Process "$browserURL"
+    $googleDriveID = $browserURL.substring(31)
+    $googleDriveID = $googleDriveID -replace ("&export=download", "")
+    Write-Host "Google Drive ID = $googleDriveID";
 
-# -.-.-.-.-.-.-.-.-.-.-.-.-
-# Wait a bit so google doesnt block you
-# -.-.-.-.-.-.-.-.-.-.-.-.-
-Start-Sleep -Seconds 1.6
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
+    # Open browser with the link                        OPTIONAL
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
+
+    # Start-Process "$browserURL"
+
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
+    # Download via Gdrive utility
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
+
+    invoke-expression "cmd /c start powershell -Command { .\gdrive.exe download $googleDriveID; Pause}"
+    
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
+    # Wait a bit so google doesnt block you
+    # -.-.-.-.-.-.-.-.-.-.-.-.-
+    Start-Sleep -Seconds 1.6
 }
+
+Pause
--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 Windows 10 powershell script to automatically decode the Base64 link and open the url in your browser
 
 ## Dependencies
-- [x] 7-Zip, You will need it installed on your Windows, the script will find it automatically
-- [x] Powershell, duh
+- [7-Zip](https://www.7-zip.org/download.html), You will need it installed on your Windows, the script will find it automatically
+- [Google Drive CLI Client](https://github.com/gdrive-org/gdrive)
+- Powershell, duh
 
 ## Usage
-Download the files from the GetFreeCourses webpage and run the script in your browser's download directory
+1. Download the files from the GetFreeCourses webpage
+2. Download the Google Drive CLI and rename it as ``gdrive.exe``
+3. Download the script and it in your browser's download directory
 
 ``` WARNING: It will try to execute for every .zip file in your directory, so it's not my problem if you end up with missing files or problems, the script is AS-IS ```
 
@@ -14,3 +17,4 @@ Download the files from the GetFreeCourses webpage and run the script in your br
 - [ ] Parameters so it automatically deletes the original file too
 - [ ] Error detection (like 7-Zip missing from the system / stop when an error pops out)
 - [ ] Make a summary on what failed and what was a success
+- [ ] Download automatically dependencies (Google drive CLI)
